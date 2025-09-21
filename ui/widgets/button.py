2d34f03f186dddf7526d9a8b4f36e459de51f303
Navodyapiumanthi <navodyapiumanthi@gmail.com>
2025-09-22 02:42:34 +0930

    Update UI button

--- ui/widgets/button.py
+++ ui/widgets/button.py
@@ -10,15 +10,17 @@ class Button(BaseWidget):
         kwargs.setdefault("fg", theme.TEXT_COLOR)
         kwargs.setdefault("highlightbackground", theme.BG_COLOR)
         kwargs.setdefault("activebackground", theme.BUTTON_COLOR)
+        kwargs.setdefault("relief", "raised")
 
-        self.widget = tk.Button(parent, text=text, relief="flat", command=command, **kwargs)
+        self.widget = tk.Button(parent, text=text, command=command, **kwargs)
         self.widget.bind("<Enter>", self.on_enter)
         self.widget.bind("<Leave>", self.on_leave)
+        
 
     def on_enter(self, event):
-        self.widget['background'] = '#d1e7dd'  # or any color you like for hover
-        self.widget['relief'] = 'raised'
+        self.widget['background'] = "#808080"  # or any color you like for hover
+        
 
     def on_leave(self, event):
         self.widget['background'] = theme.BUTTON_COLOR
-        self.widget['relief'] = 'flat'
+        
